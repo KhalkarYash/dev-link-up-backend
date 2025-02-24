48b950225b3180416c64f8d551d069bd3915a121
KhalkarYash <yashmk2004@gmail.com>
2025-02-24 20:27:54 +0530

    Exploring GET, POST, DELETE

--- src/app.js
+++ src/app.js
@@ -2,17 +2,25 @@ const express = require("express");
 
 const app = express();
 
+app.use("/user", (req, res) => {
+    res.send("Hahahahahahahahaha");
+})
+
+app.get("/user", (req, res) => {
+    res.send({firstName: "Yash", lastName: "Khalkar"});
+})
+
+app.post("/user", (req, res) => {
+    console.log("Data send logic here")
+    res.send("Data successfully saved in the database")
+})
+
+app.delete("/user", (req, res) => {
+    res.send("Deleted Sucessfully")
+})
 
 app.use("/hello",(req, res) => {
     res.send("Hello hello hello");
-})
-
-app.use("/test",(req, res) => {
-    res.send("Hello from the test");
-})
-
-app.use("/",(req, res) => {
-    res.send("Namaste!");
 })
 
 app.listen(7777, () => {
