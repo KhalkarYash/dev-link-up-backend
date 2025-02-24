ba70c42a86d8e6ba8b106265b185313f12cf8826
KhalkarYash <yashmk2004@gmail.com>
2025-02-24 20:45:08 +0530

    Routing and Request Handlers

--- src/app.js
+++ src/app.js
@@ -2,27 +2,12 @@ const express = require("express");
 
 const app = express();
 
-app.use("/user", (req, res) => {
-    res.send("Hahahahahahahahaha");
-})
-
-app.get("/user", (req, res) => {
-    res.send({firstName: "Yash", lastName: "Khalkar"});
-})
-
-app.post("/user", (req, res) => {
-    console.log("Data send logic here")
-    res.send("Data successfully saved in the database")
-})
-
-app.delete("/user", (req, res) => {
-    res.send("Deleted Sucessfully")
-})
-
-app.use("/hello",(req, res) => {
-    res.send("Hello hello hello");
-})
+// req /user , /user/xyz , /user/1
+app.get("/user/:userId/:name/:password", (req, res) => {
+  console.log(req.params);
+  res.send({ firstName: "Yash", lastName: "Khalkar" });
+});
 
 app.listen(7777, () => {
-    console.log("Server is running on port 7777");
+  console.log("Server is running on port 7777");
 });
