7ce7df38c7579a7d817177b554fcdfbbb425cf84
KhalkarYash <yashmk2004@gmail.com>
2025-02-25 22:06:11 +0530

    Diving into the APIs

--- src/app.js
+++ src/app.js
@@ -1,18 +1,14 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const { connectDB } = require("./config/database");
 const User = require("./models/user");
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
   // Creating a new instance of the user model
-
-  const user = new User({
-    firstName: "Y",
-    lastName: "K",
-    email: "yk@example.com",
-    password: "yk@123",
-  });
+  const user = new User(req.body);
 
   try {
     await user.save();
@@ -23,6 +19,54 @@ app.post("/signup", async (req, res) => {
     } else {
       res.status(400).send("Error occurred: " + err.message);
     }
+  }
+});
+
+// Get user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.email;
+  try {
+    const users = await User.findOne({ email: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong " + err.message);
+  }
+});
+
+// Feed API - GET /feed - get all the users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong " + err.message);
+  }
+});
+
+// Delete a user from the database
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete({ _id: userId });
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// Update a user from the database
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const users = await User.findByIdAndUpdate({ _id: userId }, data);
+    res.send("User Updated Successfully!");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
   }
 });
 
