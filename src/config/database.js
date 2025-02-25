c4abf18e087f90bfb4b5bbdf03a749433ed6d8f6
KhalkarYash <yashmk2004@gmail.com>
2025-02-25 19:13:46 +0530

    Add dotenv dependency and configure environment variables

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  mongoose.connect(
-    ""
-  );
+  mongoose.connect(process.env.MONGO_URL + process.env.MONGO_DB);
 };
 
 module.exports = {
-    connectDB
-}
+  connectDB,
+};
